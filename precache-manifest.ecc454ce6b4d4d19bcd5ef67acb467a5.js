7122456f13922e2b1400b7f0cdfb79724387d3a9
Serhii Tyshchenko <serhii.tyshchenko@gmail.com>
2020-05-31 15:28:06 +0300

    Updates

---
+++ precache-manifest.ecc454ce6b4d4d19bcd5ef67acb467a5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b7f748a87c83e4645fe032af53adba6",
+    "url": "/ShoppingList/index.html"
+  },
+  {
+    "revision": "e36699485b0fccf128b8",
+    "url": "/ShoppingList/static/css/main.6861e243.chunk.css"
+  },
+  {
+    "revision": "6bc23b641d7935ef2425",
+    "url": "/ShoppingList/static/js/2.18e0798c.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/ShoppingList/static/js/2.18e0798c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e36699485b0fccf128b8",
+    "url": "/ShoppingList/static/js/main.da535464.chunk.js"
+  },
+  {
+    "revision": "11703d59f0c6ed869ff9",
+    "url": "/ShoppingList/static/js/runtime-main.5e96c501.js"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/ShoppingList/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "9f5a9f3ffc9bb0acda6c3ea05655571a",
+    "url": "/ShoppingList/static/media/fontello.9f5a9f3f.ttf"
+  }
+]);
