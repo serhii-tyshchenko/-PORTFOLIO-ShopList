dd6afc6d4d9baf0f9aeebd2a453330a6c04cfb9e
Serhii Tyshchenko <serhii.tyshchenko@gmail.com>
2020-06-05 21:49:16 +0300

    Updates

--- precache-manifest.2afeaa648eddaf92b2b777707abb42a2.js
+++ precache-manifest.2afeaa648eddaf92b2b777707abb42a2.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6b7f748a87c83e4645fe032af53adba6",
+    "revision": "1ec8607ea2e31a7f252273b613c823e4",
     "url": "/ShoppingList/index.html"
   },
   {
-    "revision": "e36699485b0fccf128b8",
-    "url": "/ShoppingList/static/css/main.6861e243.chunk.css"
+    "revision": "cfe0bff8605490cedc6e",
+    "url": "/ShoppingList/static/css/main.b1e6c48a.chunk.css"
   },
   {
-    "revision": "6bc23b641d7935ef2425",
-    "url": "/ShoppingList/static/js/2.18e0798c.chunk.js"
+    "revision": "83416525f6315bb0f8fb",
+    "url": "/ShoppingList/static/js/2.b380d865.chunk.js"
   },
   {
     "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
-    "url": "/ShoppingList/static/js/2.18e0798c.chunk.js.LICENSE.txt"
+    "url": "/ShoppingList/static/js/2.b380d865.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e36699485b0fccf128b8",
-    "url": "/ShoppingList/static/js/main.da535464.chunk.js"
+    "revision": "cfe0bff8605490cedc6e",
+    "url": "/ShoppingList/static/js/main.3ca8bcbe.chunk.js"
   },
   {
     "revision": "11703d59f0c6ed869ff9",
@@ -30,5 +30,9 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "9f5a9f3ffc9bb0acda6c3ea05655571a",
     "url": "/ShoppingList/static/media/fontello.9f5a9f3f.ttf"
+  },
+  {
+    "revision": "3b8e113c020a1fe10dd0eb50df769c7d",
+    "url": "/ShoppingList/static/media/logo.3b8e113c.svg"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ShoppingList/precache-manifest.ecc454ce6b4d4d19bcd5ef67acb467a5.js"
+  "/ShoppingList/precache-manifest.2afeaa648eddaf92b2b777707abb42a2.js"
 );
 
 self.addEventListener('message', (event) => {
